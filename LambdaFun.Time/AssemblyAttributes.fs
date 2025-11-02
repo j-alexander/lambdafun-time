6bfbf337fe147031594e0b6268ae17090b6c14a9
Jonathan A. Leaver <jleaver@alumni.uwo.ca>
2025-11-02 12:51:18 -0500

    added assembly attributes file to replace the autogenerated one that was blocking source link

---
+++ LambdaFun.Time/AssemblyAttributes.fs
@@ -0,0 +1,13 @@
+﻿namespace LambdaFun.Time
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("LambdaFun.Time")>]
+[<assembly: AssemblyCompanyAttribute("University of Western Ontario - Artificial Intelligence Lab")>]
+[<assembly: AssemblyProductAttribute("LambdaFun.Time")>]
+[<assembly: AssemblyCopyrightAttribute("Copyright (c) 2009 Jonathan Leaver, MSc")>]
+[<assembly: AssemblyDescriptionAttribute("High precision time for measurement of distributed answer set solving benchmarks.")>]
+[<assembly: AssemblyMetadataAttribute("Authors", "Jonathan Leaver, MSc")>]
+[<assembly: AssemblyVersionAttribute("1.2.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.2.0.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("1.2.0.0")>]
+do ()
